8791a58970799f373195b5b4b1cf1f077f3d2c61
divyam0-0 <71850723+divyam0-0@users.noreply.github.com>
2025-10-21 14:10:52 +0530

    Update find_title_via_pymupdf.py

--- pdf2doi/find_title_via_pymupdf.py
+++ pdf2doi/find_title_via_pymupdf.py
@@ -1,56 +1,43 @@
-# This code has been adapted from here https://github.com/LouisdeBruijn/Medium/blob/master/PDF%20retrieval/pdf_retrieval.py
-# The Entry Point is the function find_title_via_pymupdf (defined at the bottom)
-# Try to find a title of a pdf file by parsing the file and looking at html-like elements that start with <h1>
+# PDFMiner version of the original PyMuPDF code
+# Entry point remains find_title_via_pymupdf
 
 from operator import itemgetter
-import fitz
-    
-def fonts(doc, granularity=False):
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextBoxHorizontal, LTChar
+
+def fonts(file, granularity=False):
     styles = {}
     font_counts = {}
-    for page in doc:
-        blocks = page.get_text('dict')['blocks']
-        for b in blocks:  # iterate through the text blocks
-            if b['type'] == 0:  # block contains text
-                for l in b["lines"]:  # iterate through the text lines
-                    for s in l["spans"]:  # iterate through the text spans
-                        if granularity:
-                            identifier = "{0}_{1}_{2}_{3}".format(s['size'], s['flags'], s['font'], s['color'])
-                            styles[identifier] = {'size': s['size'], 'flags': s['flags'], 'font': s['font'],
-                                                    'color': s['color']}
-                        else:
-                            identifier = "{0}".format(s['size'])
-                            styles[identifier] = {'size': s['size'], 'font': s['font']}
-    
-                        font_counts[identifier] = font_counts.get(identifier, 0) + 1  # count the fonts usage
-    
+
+    for page_layout in extract_pages(file):
+        for element in page_layout:
+            if isinstance(element, LTTextBoxHorizontal):
+                for line in element:
+                    for char in line:
+                        if isinstance(char, LTChar):
+                            if granularity:
+                                identifier = f"{char.size}_{char.fontname}_{char.ncs}"
+                                styles[identifier] = {'size': char.size, 'font': char.fontname, 'color': char.ncs}
+                            else:
+                                identifier = f"{char.size}"
+                                styles[identifier] = {'size': char.size, 'font': char.fontname}
+
+                            font_counts[identifier] = font_counts.get(identifier, 0) + 1
+
     font_counts = sorted(font_counts.items(), key=itemgetter(1), reverse=True)
-    
+
     if len(font_counts) < 1:
         raise ValueError("Zero discriminating fonts found!")
-    
+
     return font_counts, styles
-    
-    
+
 def font_tags(font_counts, styles):
-    """Returns dictionary with font sizes as keys and tags as value.
-    :param font_counts: (font_size, count) for all fonts occuring in document
-    :type font_counts: list
-    :param styles: all styles found in the document
-    :type styles: dict
-    :rtype: dict
-    :return: all element tags based on font-sizes
-    """
-    p_style = styles[font_counts[0][0]]  # get style for most used font by count (paragraph)
-    p_size = p_style['size']  # get the paragraph's size
-    
-    # sorting the font sizes high to low, so that we can append the right integer to each tag
-    font_sizes = []
-    for (font_size, count) in font_counts:
-        font_sizes.append(float(font_size))
+    p_style = styles[font_counts[0][0]]  # most used font
+    p_size = p_style['size']
+
+    font_sizes = [float(font_size) for font_size, _ in font_counts]
     font_sizes.sort(reverse=True)
-    
-    # aggregating the tags for each font size
+
     idx = 0
     size_tag = {}
     for size in font_sizes:
@@ -59,70 +46,56 @@ def font_tags(font_counts, styles):
             idx = 0
             size_tag[size] = '<p>'
         if size > p_size:
-            size_tag[size] = '<h{0}>'.format(idx)
+            size_tag[size] = f'<h{idx}>'
         elif size < p_size:
-            size_tag[size] = '<s{0}>'.format(idx)
-    
+            size_tag[size] = f'<s{idx}>'
+
     return size_tag
-    
-def headers_para(doc, size_tag):
-    """Scrapes headers & paragraphs from PDF and return texts with element tags.
-    :param doc: PDF document to iterate through
-    :type doc: <class 'fitz.fitz.Document'>
-    :param size_tag: textual element tags for each size
-    :type size_tag: dict
-    :rtype: list
-    :return: texts with pre-prended element tags
-    """
-    header_para = []  # list with headers and paragraphs
-    first = True  # boolean operator for first header
-    previous_s = {}  # previous span
-    
-    for page in doc:
-        blocks = page.get_text("dict")["blocks"]
-        for b in blocks:  # iterate through the text blocks
-            if b['type'] == 0:  # this block contains text
-    
-                # REMEMBER: multiple fonts and sizes are possible IN one block
-    
-                block_string = ""  # text found in block
-                for l in b["lines"]:  # iterate through the text lines
-                    for s in l["spans"]:  # iterate through the text spans
-                        if s['text'].strip():  # removing whitespaces:
+
+def headers_para(file, size_tag):
+    header_para = []
+    first = True
+    previous_s = {}
+
+    for page_layout in extract_pages(file):
+        for element in page_layout:
+            if isinstance(element, LTTextBoxHorizontal):
+                block_string = ""
+                for line in element:
+                    for char in line:
+                        if isinstance(char, LTChar):
+                            text = char.get_text().strip()
+                            if not text:
+                                continue
+
                             if first:
-                                previous_s = s
+                                previous_s = char
                                 first = False
-                                block_string = size_tag[s['size']] + s['text']
+                                block_string = size_tag[char.size] + text
                             else:
-                                if s['size'] == previous_s['size']:
-    
+                                if char.size == previous_s.size:
                                     if block_string and all((c == "|") for c in block_string):
-                                        # block_string only contains pipes
-                                        block_string = size_tag[s['size']] + s['text']
-                                    if block_string == "":
-                                        # new block has started, so append size tag
-                                        block_string = size_tag[s['size']] + s['text']
-                                    else:  # in the same block, so concatenate strings
-                                        block_string += " " + s['text']
-    
+                                        block_string = size_tag[char.size] + text
+                                    elif block_string == "":
+                                        block_string = size_tag[char.size] + text
+                                    else:
+                                        block_string += " " + text
                                 else:
                                     header_para.append(block_string)
-                                    block_string = size_tag[s['size']] + s['text']
-    
-                                previous_s = s
-    
-                    # new block started, indicating with a pipe
+                                    block_string = size_tag[char.size] + text
+
+                                previous_s = char
+
                     block_string += "|"
-    
+
                 header_para.append(block_string)
-    
+
     return header_para
 
 def find_title_via_pymupdf(file):
-    doc = fitz.open(file)
-    font_counts, styles = fonts(doc, granularity=False)
+    font_counts, styles = fonts(file, granularity=False)
     size_tag = font_tags(font_counts, styles)
-    elements = headers_para(doc, size_tag)
+    elements = headers_para(file, size_tag)
     for e in elements:
         if e.startswith('<h1>'):
-            return (e.lstrip("<h1>")).replace("|", "")
+            return e.lstrip("<h1>").replace("|", "")
